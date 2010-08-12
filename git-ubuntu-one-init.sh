c5d7e939645b713e0871d6aeb2a9c4d885ef3531
Ramiro Gomez <web@ramiro.org>
2010-08-12 22:42:46 +0200

    git helpers

---
+++ git-ubuntu-one-init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# variables
+GITREPOS=$HOME'/Ubuntu One/repos'
+PROJECTDIR=`pwd`
+PROJECT=`basename $PROJECTDIR`
+PROJECTREPO=$GITREPOS/$PROJECT.git
+
+echo "Init git in project dir $PROJECTDIR"
+git init
+git add .
+git commit -m "first commit"
+
+echo "Create git repository for project in $PROJECTREPO"
+mkdir "$PROJECTREPO"
+cd "$PROJECTREPO"
+git init --bare
+
+echo "Push to project repository"
+cd "$PROJECTDIR"
+git remote add origin "$PROJECTREPO"
+git push origin master
