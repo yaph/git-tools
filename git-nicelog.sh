7a02f043059026cd81476f54713dc8b3a60b2a42
Ramiro Gómez <code@ramiro.org>
2013-12-02 11:14:46 +0100

    added source

--- git-nicelog.sh
+++ git-nicelog.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+# from https://github.com/luanmuniz/git-tools/blob/master/git-nicelog.sh
 git log --pretty="%C(magenta)%h %C(yellow)%ad %C(white)| %C(green)%s%C(red)%d %C(yellow)[%an]" --graph --date-order --date=short --max-count=15
