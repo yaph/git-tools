6690d15c90171f5f0203ea2abc65427c59f3c6a1
Ramiro Gómez <code@ramiro.org>
2023-07-12 23:08:35 +0200

    Add optional argument to set project name

--- git-local-init.sh
+++ git-local-init.sh
@@ -1,20 +1,26 @@
-#!/bin/sh
-GITREPOS=/media/rg/Volume/repos/remotes
-PROJECTDIR=`pwd`
-PROJECT=`basename $PROJECTDIR`
-PROJECTREPO=$GITREPOS/$PROJECT.git
+#!/bin/bash
+# Initialize a git repository from the current working directory.
+# If called a with an argument it will be used as the repo name.
+set -euo pipefail
 
-echo "Init git in project dir $PROJECTDIR"
+DIR=`pwd`
+PROJECT=${1:-`basename $DIR`}
+REPO=$GITREMOTES/$PROJECT.git
+
+echo $REPO
+exit
+
+echo "Init git in project dir $DIR"
 git init
 git add .
 git commit -m "first commit"
 
-echo "Create git repository for project in $PROJECTREPO"
-mkdir "$PROJECTREPO"
-cd "$PROJECTREPO"
+echo "Create git repository for project in $REPO"
+mkdir "$REPO"
+cd "$REPO"
 git init --bare
 
 echo "Push to project repository"
-cd "$PROJECTDIR"
-git remote add origin "$PROJECTREPO"
+cd "$DIR"
+git remote add origin "$REPO"
 git push origin main
