15217b32621907b300bbd90f8e526120c80d6b4d
Ramiro Gómez <code@ramiro.org>
2023-10-30 12:00:55 +0100

    Don't push to GitHub repos not owned by specified user

--- git-update-repos.sh
+++ git-update-repos.sh
@@ -2,6 +2,8 @@
 # Update (pull and push) all Git repositories found in the current directory and its subdirectories.
 set -euo pipefail
 
+gh_user="yaph"
+
 # Find all .git directories in the current directory and subdirectories
 git_dirs=$(find "$(pwd)" -name .git -type d)
 
@@ -14,7 +16,14 @@ for git_dir in $git_dirs; do
         echo "Updating Git repository in $repo_dir"
         cd "$repo_dir" || continue
         git pull
-        git push
+
+        remote=$(git remote get-url origin)
+        if [[ "$remote" == "git@github.com"* ]] && [[ "$remote" != "git@github.com:$gh_user"* ]]; then
+            echo "Don't push to $remote. Continuing..."
+        else
+            echo "Run git push"
+            git push
+        fi
     else
         echo "Skipping $repo_dir - not a Git repository"
     fi
